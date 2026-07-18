9ef889dc69abe86d0662b22d4d0c8ae80ee70b3b
termul488-ai <termul488@gmail.com>
2026-07-18 22:51:01 +0700

    Update allin-one.py

--- allin-one.py
+++ allin-one.py
@@ -175,7 +175,7 @@ def http_flood(url, duration):
 def validate_input(prompt, min_val, max_val, input_type=int):
     while True:
         try:
-            value = input_type(input(Fore.LIGHTBLUE_EX + prompt))
+            value = input_type(input(prompt))
             if min_val <= value <= max_val:
                 return value
             print(f"[!] Value must be between {min_val} and {max_val}.")
